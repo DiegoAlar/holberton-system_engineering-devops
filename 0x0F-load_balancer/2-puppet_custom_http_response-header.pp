9643d3102f955fd667f3f7d0054e1a718a6f38d9
DiegoAlar <diegoaav@hotmail.com>
2020-04-03 19:13:49 -0500

    adding task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,4 +1,5 @@
 # creating a custom HTTP header response, but with Puppet
 exec { 'conf_server':
-command => 'sudo apt-get update && sudo apt-get install -y nginx && && echo "Holberton School" > /var/www/html/index.nginx-debian.html && sudo chmod -R 777 /etc/nginx && sudo chmod -R 777 /usr/share/nginx/html/ && sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && echo "Ceci n"'"est pas une page'"' > /usr/share/nginx/html/custom_404.html && sed -i '"'/sendfile on;/a add_header X-Served-By $HOSTNAME;'"' /etc/nginx/nginx.conf && sudo service nginx restart',
+command  => 'sudo apt-get update && sudo apt-get install -y nginx && && echo "Holberton School" > /var/www/html/index.nginx-debian.html && sudo chmod -R 777 /etc/nginx && sudo chmod -R 777 /usr/share/nginx/html/ && sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && echo "Ceci n"'"est pas une page'"' > /usr/share/nginx/html/custom_404.html && sed -i '"'/sendfile on;/a add_header X-Served-By $HOSTNAME;'"' /etc/nginx/nginx.conf && sudo service nginx restart',
+provider => shell,
 }
