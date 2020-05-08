69c9e7f0ee9bf3921d9e1fb089f1206f39106400
DiegoAlar <diegoaav@hotmail.com>
2020-05-07 21:23:07 -0500

    fixing 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,8 +13,10 @@ def count_words(subreddit, word_list, after='', a_dict=None):
         a_dict = {}
         a_list = []
         for item in word_list:
-            if re.search('_', item) or item in a_dict:
+            if re.search('_', item):
                 pass
+            elif item.lower() in a_dict:
+                a_dict[item] += 1
             else:
                 a_dict[item.lower()] = 0
                 a_list.append(item.lower())
@@ -24,19 +26,23 @@ def count_words(subreddit, word_list, after='', a_dict=None):
     headers = {'user-agent': 'X-Modhash'}
     req = requests.get(url, headers=headers)
     if req.history or req.status_code == 404:
-        return None
-    data = req.json()
-    next_after = data.get('data').get('after')
-    if next_after:
-        req = data.get('data').get('children')
-        for title in req:
-            title = title.get('data').get('title')
-            for word in word_list:
-                if re.search(
-                        r'\b{}\b'.format(word.lower()), title, re.IGNORECASE):
-                    a_dict[word] += 1
-        count_words(subreddit, word_list, next_after, a_dict)
+        print()
     else:
-        sorted_dict = sorted(a_dict.items(), key=lambda x: x[1], reverse=True)
-        for item in sorted_dict:
-            print('{}: {}'.format(item[0], item[1]))
+        data = req.json()
+        next_after = data.get('data').get('after')
+        if next_after:
+            req = data.get('data').get('children')
+            for title in req:
+                title = title.get('data').get('title')
+                for word in word_list:
+                    if re.search(
+                            r'\b{}\b'.format(
+                                word.lower()), title, re.IGNORECASE):
+                        a_dict[word] += 1
+            count_words(subreddit, word_list, next_after, a_dict)
+        else:
+            sorted_dict = sorted(
+                a_dict.items(), key=lambda x: x[1], reverse=True)
+            for item in sorted_dict:
+                if item[1] != 0:
+                    print('{}: {}'.format(item[0], item[1]))
